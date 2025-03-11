b264cf8186b13c0fd050f377faff929fb79e2ad9
Johnny <1602335223@qq.com>
2025-03-11 20:58:24 +0800

    提交

--- JavaBasic/src/main/java/org/example/threads/BasicThreads.java
+++ JavaBasic/src/main/java/org/example/threads/BasicThreads.java
@@ -1,5 +1,8 @@
 package org.example.threads;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class BasicThreads {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++){
@@ -7,6 +10,7 @@ public class BasicThreads {
 
             System.out.println("Waiting for LiftOff");
         }
+
 
     }
 }
